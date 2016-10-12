a4ae062c69f9f4d12b367792f0a267f1794309bc
Christina <Christina@MacBook-Air-Hristina.local>
2016-10-12 19:50:04 +0300

    design

---
+++ Frontend/src/pizza/PizzaCart.js
@@ -0,0 +1,108 @@
+/**
+ * Created by chaika on 02.02.16.
+ */
+var Templates = require('../Templates');
+var Templates = require('./storage');
+
+//Перелік розмірів піци
+var PizzaSize = {
+    Big: "big_size",
+    Small: "small_size"
+};
+
+//Змінна в якій зберігаються перелік піц в кошику
+var Cart = [];
+var saveCart = storage.get('card', Card);
+
+if(saveCard){
+    Cart = saveCart;
+}
+
+//HTML едемент куди будуть додаватися піци
+var $cart = $("#cart");
+
+function addToCart(pizza, size) {
+    //Додавання однієї піци в кошик покупок
+
+    //Приклад реалізації, можна робити будь-яким іншим способом
+    Cart.push({
+        pizza: pizza,
+        size: size,
+        quantity: 1
+    });
+
+    //Оновити вміст кошика на сторінці
+    updateCart();
+}
+
+function removeFromCart(cart_item) {
+    //Видалити піцу з кошика
+    //TODO: треба зробити
+    var index = Card.index(cart_item);
+    if(index>-1){
+        Cart.splice(index,1)
+    }
+    //Після видалення оновити відображення
+    updateCart();
+}
+
+function removeCart(cart_item) {
+    //Видалити піцу з кошика
+    //TODO: треба зробити
+
+    //Після видалення оновити відображення
+    updateCart();
+}
+
+function initialiseCart() {
+    //Фукнція віпрацьвуватиме при завантаженні сторінки
+    //Тут можна наприклад, зчитати вміст корзини який збережено в Local Storage то показати його
+    //TODO: ...
+
+    $('.clean-cart').click(cleancCard)
+    updateCart();
+}
+
+function getPizzaInCart() {
+    //Повертає піци які зберігаються в кошику
+    return Cart;
+}
+
+function updateCart() {
+    //Функція викликається при зміні вмісту кошика
+    //Тут можна наприклад показати оновлений кошик на екрані та зберегти вміт кошика в Local Storage
+    
+    
+    //Очищаємо старі піци в кошику
+    $cart.html("");
+
+    //Онволення однієї піци
+    function showOnePizzaInCart(cart_item) {
+        var html_code = Templates.PizzaCart_OneItem(cart_item);
+
+        var $node = $(html_code);
+
+        $node.find(".plus").click(function(){
+            //Збільшуємо кількість замовлених піц
+            cart_item.quantity += 1; 
+        });
+        $node.find(".delete-item").click(function(){
+            removeFromCart(cart-item);
+    
+        });
+
+        $cart.append($node);
+    }
+
+    Cart.forEach(showOnePizzaInCart);
+    storage.set('cart', Cart)
+
+}
+
+exports.removeFromCart = removeFromCart;
+exports.addToCart = addToCart;
+
+exports.getPizzaInCart = getPizzaInCart;
+exports.initialiseCart = initialiseCart;
+
+exports.PizzaSize = PizzaSize;
